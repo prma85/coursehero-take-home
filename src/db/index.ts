39ebcdbb4625a70cd5526311351a7270a30224bd
Paulo Andrade <paulo85br@gmail.com>
2021-02-18 20:19:01 -0600

    db changes

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,5 +5,6 @@ export interface Course {
   semester: string;
 }
 
+// Just creating a pseudo API call for a local database
 export const getCourseList = (): Promise<Course[]> =>
   fetch("./data.json").then((data) => data.json());
